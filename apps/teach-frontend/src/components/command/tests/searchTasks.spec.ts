080f42a470b4399f2258d87d16fd3bd594cf710e
sundada <956593305@qq.com>
2023-06-23 21:04:20 +0800

    feat: add test for searching task

--- apps/teach-frontend/src/components/command/tests/searchTasks.spec.ts
+++ apps/teach-frontend/src/components/command/tests/searchTasks.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 
 import { filteredTasks, resetSearchTasks, searchTasks } from '../searchTasks'
+import { completeSmartProject } from '@/store'
 vi.mock('@/store/tasks')
 vi.mock('@/store/listProjects')
 
@@ -8,8 +9,64 @@ describe('test searchTasks', () => {
   beforeEach(() => {
     resetSearchTasks()
   })
-  it('should', async () => {
-    await searchTasks('test')
-    expect(filteredTasks).toMatchInlineSnapshot()
+  it('search task by title', async () => {
+    const title = '吃饭'
+    await searchTasks(title)
+    expect(filteredTasks.value[0].item.title).toBe(title)
+  })
+  it('search task by description', async () => {
+    const description = '睡觉'
+    await searchTasks(description)
+    expect(filteredTasks.value[0].item.desc).toBe('睡觉能治愈一切')
+  })
+  it('should be empty when input don\'t match char', async () => {
+    const title = '社畜'
+    await searchTasks(title)
+    expect(filteredTasks.value.length).toBe(0)
+  })
+  it('should be smartProject when task\'s status is completed', async () => {
+    const title = '睡觉'
+    await searchTasks(title)
+    expect(filteredTasks.value).toMatchInlineSnapshot(`
+      [
+        {
+          "item": {
+            "desc": "睡觉能治愈一切",
+            "done": false,
+            "from": {
+              "id": "1",
+              "name": "test 集合",
+              "type": "listProject",
+            },
+            "id": "2",
+            "title": "睡觉",
+          },
+          "refIndex": 1,
+        },
+      ]
+    `)
+    expect(filteredTasks.value[0].item.from!.type).toBe('listProject')
+  })
+  it('should be smartProject when task\'s status is completed', async () => {
+    const title = '打豆豆'
+    await searchTasks(title)
+    expect(filteredTasks.value).toMatchInlineSnapshot(`
+      [
+        {
+          "item": {
+            "desc": "打的不是豆豆，是寂寞",
+            "done": true,
+            "from": {
+              "name": "已完成",
+              "type": "smartProject",
+            },
+            "id": "3",
+            "title": "打豆豆",
+          },
+          "refIndex": 2,
+        },
+      ]
+    `)
+    expect(filteredTasks.value[0].item.from!.type).toBe(completeSmartProject.type)
   })
 })
--- apps/teach-frontend/src/store/__mocks__/tasks.ts
+++ apps/teach-frontend/src/store/__mocks__/tasks.ts
@@ -1,29 +1,30 @@
 import { vi } from 'vitest'
 import type { Task } from '../tasks'
 import { TaskStatus } from '../tasks'
+export { TaskStatus } from '../tasks'
 
 const tasks: Task[] = [
   {
     id: '1',
-    title: 'test1',
+    title: '吃饭',
     status: TaskStatus.ACTIVE,
-    content: '这是第一条测试数据 test1',
+    content: '干啥啥不行，吃饭第一名',
     projectId: '1',
     position: 0,
   },
   {
     id: '2',
-    title: 'test2',
+    title: '睡觉',
     status: TaskStatus.ACTIVE,
-    content: '这是第一条测试数据 test2',
+    content: '睡觉能治愈一切',
     projectId: '1',
     position: 0,
   },
   {
     id: '3',
-    title: 'test3',
-    status: TaskStatus.ACTIVE,
-    content: '这是第一条测试数据 test3',
+    title: '打豆豆',
+    status: TaskStatus.COMPLETED,
+    content: '打的不是豆豆，是寂寞',
     projectId: '1',
     position: 0,
   },
